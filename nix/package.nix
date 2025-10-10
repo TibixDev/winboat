2af9a180c69abd44291c63fcef6148e258c2408f
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-10-10 13:53:42 +0400

    nix: refactored package.nix + hardcode version

--- nix/package.nix
+++ nix/package.nix
@@ -11,11 +11,15 @@
   copyDesktopItems,
   asar,
   libusb1,
-}: let
-  packageJson = builtins.fromJSON (builtins.readFile ../package.json);
-  inherit (packageJson) version;
+}:
+stdenv.mkDerivation (final: {
+  pname = "winboat";
+  version = "0.8.7";
 
-  iconFile = ../icons/icon.png;
+  src = fetchurl {
+    url = "https://github.com/TibixDev/winboat/releases/download/v${final.version}/winboat-${final.version}-x64.tar.gz";
+    sha256 = "sha256-4NV9nyFLYJt9tz3ikDTb1oSpJGAKr1I49D0VHqpty3I=";
+  };
 
   desktopItems = [
     (makeDesktopItem {
@@ -28,66 +32,57 @@
       categories = ["Utility"];
     })
   ];
-in
-  stdenv.mkDerivation {
-    inherit desktopItems version;
-    pname = "winboat";
 
-    src = fetchurl {
-      url = "https://github.com/TibixDev/winboat/releases/download/v${version}/winboat-${version}-x64.tar.gz";
-      sha256 = "sha256-4NV9nyFLYJt9tz3ikDTb1oSpJGAKr1I49D0VHqpty3I=";
-    };
+  nativeBuildInputs = [
+    makeWrapper
+    wrapGAppsHook3
+    copyDesktopItems
+    autoPatchelfHook
+    asar
+  ];
 
-    nativeBuildInputs = [
-      makeWrapper
-      wrapGAppsHook3
-      copyDesktopItems
-      autoPatchelfHook
-      asar
-    ];
+  buildInputs = [libusb1 usbutils];
 
-    buildInputs = [libusb1 usbutils];
+  dontBuild = true;
+  dontWrapGApps = true;
+  autoPatchelfIgnoreMissingDeps = ["libc.musl-x86_64.so.1"];
 
-    dontBuild = true;
-    dontWrapGApps = true;
-    autoPatchelfIgnoreMissingDeps = ["libc.musl-x86_64.so.1"];
+  installPhase = ''
+    runHook preInstall
 
-    installPhase = ''
-      runHook preInstall
+    mkdir -p $out/bin $out/share/winboat
+    cp -r ./* $out/share/winboat
+    rm $out/share/winboat/{*.so*,winboat,chrome_crashpad_handler,chrome-sandbox}
 
-      mkdir -p $out/bin $out/share/winboat
-      cp -r ./* $out/share/winboat
-      rm $out/share/winboat/{*.so*,winboat,chrome_crashpad_handler,chrome-sandbox}
+    mkdir -p $out/share/icons/hicolor/256x256/apps
+    cp ${../icons/icon.png} $out/share/icons/hicolor/256x256/apps/winboat.png
 
-      mkdir -p $out/share/icons/hicolor/256x256/apps
-      cp ${iconFile} $out/share/icons/hicolor/256x256/apps/winboat.png
+    mkdir -p $out/share/winboat/data
+    cp resources/data/usb.ids $out/share/winboat/data/usb.ids
 
-      mkdir -p $out/share/winboat/data
-      cp resources/data/usb.ids $out/share/winboat/data/usb.ids
+    mkdir -p $out/lib
+    cp -r resources/guest_server $out/lib/guest_server
+    cp -r resources/guest_server $out/share/winboat/guest_server
 
-      mkdir -p $out/lib
-      cp -r resources/guest_server $out/lib/guest_server
-      cp -r resources/guest_server $out/share/winboat/guest_server
+    # Rebuild the ASAR archive to patchelf native module.
+    tmp=$(mktemp -d)
+    asar extract $out/share/winboat/resources/app.asar $tmp
+    rm $out/share/winboat/resources/app.asar
+    autoPatchelf $tmp
+    asar pack $tmp/ $out/share/winboat/resources/app.asar
+    rm -rf $tmp
 
-      # Rebuild the ASAR archive to patchelf native module.
-      tmp=$(mktemp -d)
-      asar extract $out/share/winboat/resources/app.asar $tmp
-      rm $out/share/winboat/resources/app.asar
-      autoPatchelf $tmp
-      asar pack $tmp/ $out/share/winboat/resources/app.asar
-      rm -rf $tmp
+    makeWrapper ${electron}/bin/electron $out/bin/winboat \
+      --add-flag "$out/share/winboat/resources/app.asar" \
+      --suffix PATH : "${usbutils}/bin" \
+      ''${gappsWrapperArgs[@]}
 
-      makeWrapper ${electron}/bin/electron $out/bin/winboat \
-        --add-flag "$out/share/winboat/resources/app.asar" \
-        --suffix PATH : "${usbutils}/bin" \
-        ''${gappsWrapperArgs[@]}
+    runHook postInstall
+  '';
 
-      runHook postInstall
-    '';
-
-    meta = {
-      description = "WinBoat";
-      license = lib.licenses.mit;
-      platforms = ["x86_64-linux"];
-    };
-  }
+  meta = {
+    description = "WinBoat";
+    license = lib.licenses.mit;
+    platforms = ["x86_64-linux"];
+  };
+})
