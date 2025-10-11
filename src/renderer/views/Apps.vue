c3dc549f5529a825cea5906fd64d1fda34840328
Devin <93222657+devin-slothower@users.noreply.github.com>
2025-10-11 12:16:00 -0400

    Update Apps.vue
    Filter Icon

--- src/renderer/views/Apps.vue
+++ src/renderer/views/Apps.vue
@@ -116,7 +116,7 @@
                 >
                     <x-menu class="">
                         <x-menuitem value="all" toggled>
-                            <x-icon href="#sort" class="qualifier"></x-icon>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="qualifier" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M15 19.88c.04.3-.06.62-.29.83a.996.996 0 0 1-1.41 0L9.29 16.7a.99.99 0 0 1-.29-.83v-5.12L4.21 4.62a1 1 0 0 1 .17-1.4c.19-.14.4-.22.62-.22h14c.22 0 .43.08.62.22a1 1 0 0 1 .17 1.4L15 10.75zM7.04 5L11 10.06v5.52l2 2v-7.53L16.96 5z"/></svg>
                             <x-label>
                                 <span class="qualifier">
                                     Filter:
@@ -124,7 +124,7 @@
                                 All</x-label>
                         </x-menuitem>
                         <x-menuitem value="custom">
-                            <x-icon href="#sort" class="qualifier"></x-icon>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="qualifier" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M15 19.88c.04.3-.06.62-.29.83a.996.996 0 0 1-1.41 0L9.29 16.7a.99.99 0 0 1-.29-.83v-5.12L4.21 4.62a1 1 0 0 1 .17-1.4c.19-.14.4-.22.62-.22h14c.22 0 .43.08.62.22a1 1 0 0 1 .17 1.4L15 10.75zM7.04 5L11 10.06v5.52l2 2v-7.53L16.96 5z"/></svg>
                             <x-label>
                                 <span class="qualifier">
                                     Filter:
@@ -133,7 +133,7 @@
                             </x-label>
                         </x-menuitem>
                         <x-menuitem value="default">
-                            <x-icon href="#sort" class="qualifier"></x-icon>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="qualifier" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M15 19.88c.04.3-.06.62-.29.83a.996.996 0 0 1-1.41 0L9.29 16.7a.99.99 0 0 1-.29-.83v-5.12L4.21 4.62a1 1 0 0 1 .17-1.4c.19-.14.4-.22.62-.22h14c.22 0 .43.08.62.22a1 1 0 0 1 .17 1.4L15 10.75zM7.04 5L11 10.06v5.52l2 2v-7.53L16.96 5z"/></svg>
                             <x-label>
                                 <span class="qualifier">
                                     Filter:
