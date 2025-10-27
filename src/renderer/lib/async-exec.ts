be52a600a14e856a7fa525b2a3df7f66d7d1681d
Auri <f5bde6@proton.me>
2025-10-27 22:32:06 +0000

    Undo accidental force push

--- src/renderer/lib/async-exec.ts
+++ src/renderer/lib/async-exec.ts
@@ -1,4 +1,4 @@
-const { execFile }: typeof import("child_process") = require("child_process");
-const { promisify }: typeof import("util") = require("util");
+const { execFile }: typeof import("child_process") = require("node:child_process");
+const { promisify }: typeof import("util") = require("node:util");
 
 export const execFileAsync = promisify(execFile);
