5841080c94abede23eef76c1797a36123e2ae5df
Auri <f5bde6@proton.me>
2025-10-27 23:14:52 +0000

    Removed eff wordlist and use word-list instead

--- src/renderer/lib/password.ts
+++ src/renderer/lib/password.ts
@@ -1,7 +1,7 @@
+import wordsListPath from "word-list";
+
 const fs: typeof import("fs") = require("node:fs");
-const path: typeof import("path") = require("node:path");
 const crypto: typeof import("crypto") = require("node:crypto");
-const remote: typeof import("@electron/remote") = require("@electron/remote");
 
 export function generateRandomPassword(wordList: string[], length: number, separator: string): string {
     let result = "";
@@ -21,15 +21,6 @@ function getRandomWordFrom(words: string[]) {
 }
 
 export function readWordList(): string[] {
-    const pattern = new RegExp(" *\\d{5}\\t(\\w+) *(?:\\n|\\r\\n|\\r)", "g");
-    // TODO: I would prefer if this is something like rust's include_str!
-    const filePath = remote.app.isPackaged
-        ? path.join(process.resourcesPath, "data", "eff_large_wordlist.txt") // For packaged app
-        : path.join(remote.app.getAppPath(), "..", "..", "data", "eff_large_wordlist.txt"); // For dev mode
-    const content = fs.readFileSync(filePath);
-    let result = [];
-    for (const word of content.toString().matchAll(pattern)) {
-        result.push(word[1]);
-    }
-    return result;
+    const wordArray = fs.readFileSync(wordsListPath, "utf8").split("\n");
+    return wordArray.filter(word => word.length >= 5);
 }
