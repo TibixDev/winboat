907ca634f7cddb75336d5bef34f060291db2882b
Auri <f5bde6@proton.me>
2025-10-27 22:31:05 +0000

    Fixed requested changes

--- src/renderer/utils/getFreeRDP.ts
+++ src/renderer/utils/getFreeRDP.ts
@@ -1,8 +1,5 @@
 import { execFileAsync } from "../lib/async-exec";
 
-const { exec }: typeof import("child_process") = require("child_process");
-const { promisify }: typeof import("util") = require("util");
-
 type freeRDPWrapper = (args: string[]) => Promise<{
     stdout: string;
     stderr: string;
