126c02c7094765f20d103ee8cec80966571065a8
Auri <f5bde6@proton.me>
2025-11-03 19:38:53 +0000

    Remove shared container code

--- src/renderer/lib/containers/docker.ts
+++ src/renderer/lib/containers/docker.ts
@@ -1,8 +1,11 @@
+import { ComposeConfig } from "../../../types";
 import { DOCKER_DEFAULT_COMPOSE } from "../../data/docker";
+import { capitalizeFirstLetter } from "../../utils/capitalize";
+import { ComposePortEntry } from "../../utils/port";
 import { WINBOAT_DIR } from "../constants";
-import { ComposeDirection } from "./container";
-import { execFileAsync } from "../exec-helper";
-import { SharedContainerManagerBehaviour } from "./shared";
+import { ComposeDirection, ContainerAction, containerLogger, ContainerManager, ContainerStatus } from "./container";
+import YAML from "yaml";
+import { execFileAsync, stringifyExecFile } from "../exec-helper";
 
 const path: typeof import("path") = require("path");
 const fs: typeof import("fs") = require("fs");
@@ -15,16 +18,127 @@ export type DockerSpecs = {
 };
 
 // TODO: We probably need to separate these into their respective files.
-export class DockerContainer extends SharedContainerManagerBehaviour {
+export class DockerContainer extends ContainerManager {
     defaultCompose = DOCKER_DEFAULT_COMPOSE;
     composeFilePath = path.join(WINBOAT_DIR, "docker-compose.yml"); // TODO: If/when we support multiple VM's we need to put this in the constructor
     executableAlias = "docker";
 
+    cachedPortMappings: ComposePortEntry[] | null = null;
+
     constructor() {
         super();
     }
-    compose(direction: ComposeDirection): Promise<void> {
-        return super.compose_base(direction);
+
+    writeCompose(compose: ComposeConfig): void {
+        const composeContent = YAML.stringify(compose, { nullStr: "" });
+        fs.writeFileSync(this.composeFilePath, composeContent, { encoding: "utf-8" });
+
+        containerLogger.info(`Wrote to compose file at: ${this.composeFilePath}`);
+        containerLogger.info(`Compose file content: ${JSON.stringify(composeContent, null, 2)}`);
+    }
+
+    async compose(direction: ComposeDirection): Promise<void> {
+        const args = ["compose", "-f", this.composeFilePath, direction];
+        if (direction === "up") {
+            // Run compose in detached mode if we are running compose up TODO: maybe we need to run both in detached mode
+            args.push("-d");
+        }
+        try {
+            const { stderr } = await execFileAsync(this.executableAlias, args);
+            if (stderr) {
+                containerLogger.error(stderr);
+            }
+        } catch (e) {
+            containerLogger.error(`Failed to run compose command '${stringifyExecFile(this.executableAlias, args)}'`);
+            containerLogger.error(e);
+            throw e;
+        }
+    }
+
+    async container(action: ContainerAction): Promise<void> {
+        const args = ["container", action, this.containerName];
+        try {
+            const { stdout } = await execFileAsync(this.executableAlias, args);
+            containerLogger.info(`Container action '${action}' response: '${stdout}'`);
+        } catch (e) {
+            containerLogger.error(`Failed to run container action '${stringifyExecFile(this.executableAlias, args)}'`);
+            containerLogger.error(e);
+            throw e;
+        }
+    }
+
+    async port(): Promise<ComposePortEntry[]> {
+        const args = ["port", this.containerName];
+        const ret = [];
+
+        try {
+            const { stdout } = await execFileAsync(this.executableAlias, args);
+
+            for (const line of stdout.trim().split("\n")) {
+                const parts = line.split("->").map(part => part.trim());
+                const hostPart = parts[1];
+                const containerPart = parts[0];
+
+                ret.push(new ComposePortEntry(`${hostPart}:${containerPart}`));
+            }
+        } catch (e) {
+            containerLogger.error(`Failed to run container action '${stringifyExecFile(this.executableAlias, args)}'`);
+            containerLogger.error(e);
+            throw e;
+        }
+
+        containerLogger.info("Docker container active port mappings: ", JSON.stringify(ret));
+        this.cachedPortMappings = ret;
+        return ret;
+    }
+
+    async remove(): Promise<void> {
+        const args = ["rm", this.containerName];
+
+        try {
+            const { stdout } = await execFileAsync(this.executableAlias, args);
+        } catch (e) {
+            containerLogger.error(`Failed to remove container '${this.containerName}'`);
+            containerLogger.error(e);
+        }
+    }
+
+    async getStatus(): Promise<ContainerStatus> {
+        const statusMap = {
+            created: ContainerStatus.CREATED,
+            restarting: ContainerStatus.UNKNOWN,
+            running: ContainerStatus.RUNNING,
+            paused: ContainerStatus.PAUSED,
+            exited: ContainerStatus.EXITED,
+            dead: ContainerStatus.UNKNOWN,
+        } as const;
+        const args = ["inspect", "--format={{.State.Status}}", this.containerName];
+        try {
+            const { stdout } = await execFileAsync(this.executableAlias, args);
+            const status = stdout.trim() as keyof typeof statusMap;
+            return statusMap[status];
+        } catch (e) {
+            containerLogger.error(`Failed to get status of docker container ${e}'`);
+            return ContainerStatus.UNKNOWN;
+        }
+    }
+
+    async exists(): Promise<boolean> {
+        const args = ["ps", "-a", "--filter", `name=${this.containerName}`, "--format", "{{.Names}}"];
+        try {
+            const { stdout: exists } = await execFileAsync(this.executableAlias, args);
+            return exists.includes("WinBoat");
+        } catch (e) {
+            containerLogger.error(
+                `Failed to get container status, is ${capitalizeFirstLetter(this.executableAlias)} installed?`,
+            );
+            containerLogger.error(e);
+            return false;
+        }
+    }
+
+    get containerName(): string {
+        return this.defaultCompose.services.windows.container_name; // TODO: investigate whether we should use the compose on disk
     }
 
     static override async _getSpecs(): Promise<DockerSpecs> {
