3c95259d123374533dc7a02d858a1bc09577fa16
Kroese <kroese@users.noreply.github.com>
2025-11-06 02:46:47 +0100

    Remove USER_PORTS from install configuration
    Removed USER_PORTS from configuration.

--- src/renderer/lib/install.ts
+++ src/renderer/lib/install.ts
@@ -37,7 +37,6 @@ export const DefaultCompose: ComposeConfig = {
                 HOME: "${HOME}",
                 LANGUAGE: "English",
                 HOST_PORTS: "7149",
-                USER_PORTS: "7148",
                 ARGUMENTS: "-qmp tcp:0.0.0.0:7149,server,wait=off",
             },
             cap_add: ["NET_ADMIN"],
