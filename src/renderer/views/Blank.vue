cb4f3757c22bdecb3214d2dd4fd10a075147a258
TibixDev <tibix@fhs.sh>
2025-11-13 23:19:17 +0200

    feat: Split blank/migration view, implement a good looking migration view

--- src/renderer/views/Blank.vue
+++ src/renderer/views/Blank.vue
@@ -1,18 +1,5 @@
 <template>
     <div class="flex justify-center items-center mt-40">
-        <div v-if="useRoute().meta.migration" class="flex flex-col w-[40vw]">
-            <div class="flex flex-row items-center gap-5">
-                <h3 class="mt-2">Performing Migrations</h3>
-                <x-throbber class="w-16 h-16"></x-throbber>
-            </div>
-            <p class="max-w-[40vw]">
-                We need to update the container compose due to some changes introduced in 0.9.0     
-            </p>
-        </div>
-        <x-throbber v-else class="w-16 h-16"></x-throbber>
+        <x-throbber class="w-16 h-16"></x-throbber>
     </div>
 </template>
-
-<script setup>
-import { useRoute } from "vue-router";
-</script>
