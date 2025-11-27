7b9469a764205a4370c7b87c6c5af0128dc20f0b
Adian Kozlica <kozlicaadian@gmail.com>
2025-11-27 05:25:47 +0100

    fix: autosetup_freerdp

--- autosetup/autosetup_freerdp.sh
+++ autosetup/autosetup_freerdp.sh
@@ -4,7 +4,7 @@ source "$(dirname "$0")/common-script.sh"
 
 installFreeRdp() {
     if ! command_exists xfreerdp && ! command_exists xfreerdp3 && ! flatpak info com.freerdp.FreeRDP >/dev/null 2>&1; then
-        flatpak install -y --user flathub com.freerdp.FreeRDP
+        flatpak install -y --system flathub com.freerdp.FreeRDP
     else
         printf "%b\n" "${CYAN}FreeRDP is installed${RC}"
     fi
