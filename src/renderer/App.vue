92f2d61db47fb07a21b11cb0913384d811e6a825
LittleT <littlet6109@outlook.com>
2026-03-27 09:13:41 -0500

    feat: Add more links

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -131,20 +131,31 @@
                 <div class="flex flex-col justify-end items-center p-4 h-full">
                     <p class="text-xs text-neutral-500">WinBoat Beta v{{ appVer }} {{ isDev ? "Dev" : "Prod" }}</p>
                     <div class="flex flex-row gap-2 items-left">
-                        <a href="https://winboat.app" @click="openAnchorLink" title="Website">
+                        <a href="https://winboat.app" @click="openAnchorLink" title="Homepage">
                             <Icon class="w-8 h-8 text-neutral-500 hover:scale-110 active:scale-95 pointer-events-none" icon="mdi:web" />
                         </a>
                         <a href="https://github.com/TibixDev/winboat" @click="openAnchorLink" title="GitHub">
                             <Icon class="w-8 h-8 text-neutral-500 hover:scale-110 active:scale-95 pointer-events-none" icon="mdi:github" />
                         </a>
-                        <a href="https://ko-fi.com/winboat" @click="openAnchorLink" title="Donate">
-                            <Icon class="w-8 h-8 text-neutral-500 hover:scale-110 active:scale-95 pointer-events-none" icon="simple-icons:kofi" />
-                        </a>
                         <a href="https://discord.gg/MEwmpWm4tN" @click="openAnchorLink" title="Discord">
                             <Icon class="w-8 h-8 text-neutral-500 hover:scale-110 active:scale-95 pointer-events-none" icon="simple-icons:discord" />
                         </a>
+                    </div>
+                    <div class="flex flex-row gap-2 items-left">
+                        <a href="https://ko-fi.com/winboat" @click="openAnchorLink" title="Donate">
+                            <Icon class="w-8 h-8 text-neutral-500 hover:scale-110 active:scale-95 pointer-events-none" icon="simple-icons:kofi" />
+                        </a>
+                        <a href="https://fosstodon.org/@winboat" @click="openAnchorLink" title="Mastodon">
+                            <Icon class="w-8 h-8 text-neutral-500 hover:scale-110 active:scale-95 pointer-events-none" icon="simple-icons:mastodon" />
+                        </a>
                         <a href="https://bsky.app/profile/winboat.app" @click="openAnchorLink" title="Bluesky">
                             <Icon class="w-8 h-8 text-neutral-500 hover:scale-110 active:scale-95 pointer-events-none" icon="simple-icons:bluesky" />
+                        </a>
+                        <a href="https://x.com/winboat_app" @click="openAnchorLink" title="Twitter (X)">
+                            <Icon class="w-8 h-8 text-neutral-500 hover:scale-110 active:scale-95 pointer-events-none" icon="simple-icons:twitter" />
+                        </a>
+                        <a href="mailto:staff@winboat.app" @click="openAnchorLink" title="Email (staff@winboat.app)">
+                            <Icon class="w-8 h-8 text-neutral-500 hover:scale-110 active:scale-95 pointer-events-none" icon="simple-icons:maildotru" />
                         </a>
                     </div>
                 </div>
