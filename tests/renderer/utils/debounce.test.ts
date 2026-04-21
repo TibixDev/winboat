8930f2458130da947128d22480a3a2751377284d
KCGD <kcgdhosting@gmail.com>
2026-04-21 14:22:59 -0400

    add debounce util tests

---
+++ tests/renderer/utils/debounce.test.ts
@@ -0,0 +1,40 @@
+import { debounce } from "../../../src/renderer/utils/debounce";
+import { generateRandomLowercase } from "../../testutils";
+
+const range = [10, 1000];
+const num_tests = 10;
+const allow_miss_ms = 10;
+const args_len = 3;
+
+describe("Test debounce", () => {
+
+    // test delay of function calling
+    test("Test Delay", async () => {
+        const tests = new Array(num_tests)
+            .fill(0)
+            .map(() =>
+                Math.round(range[0] + (Math.random() * (range[1] - range[0])))
+            );
+    
+        await Promise.all(
+            tests.map(async (delay) => {
+                const T_i = Date.now();
+    
+                const T_f = await new Promise<number>((resolve) => {
+                    debounce(() => {
+                        resolve(Date.now());
+                    }, delay)();
+                });
+    
+                const T_diff = T_f - T_i;
+                expect(Math.abs(T_diff - delay)).toBeLessThanOrEqual(allow_miss_ms);
+            })
+        );
+    }, 3000);
+
+    // test arguments passthrough
+    test("Test Arguments", async() => {
+        let args = new Array(args_len).fill(0).map((i)=>Math.random());
+        await new Promise
+    })
+})
